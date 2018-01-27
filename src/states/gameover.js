65e152500fe49bd8deb024811a342bbdf0cd7738
bomberjester <bkassebaum@charter.net>
2018-01-27 11:15:34 -0600

    i took out my change

--- src/states/gameover.js
+++ src/states/gameover.js
@@ -3,7 +3,7 @@ class Menu extends Phaser.State {
   constructor() {
     super();
   }
-// making a change because i can
+
   create() {
     const text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'Gameover', {
       font: '42px Arial', fill: '#ffffff', align: 'center'
