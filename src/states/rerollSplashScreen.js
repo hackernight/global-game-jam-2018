acd6890271767e02fb0b0ad4757f2c3ed7e7fba5
Becky Asbury <rasbury7@gmail.com>
2018-01-27 23:01:10 -0600

    added depressing messages for later mulligans

--- src/states/rerollSplashScreen.js
+++ src/states/rerollSplashScreen.js
@@ -5,7 +5,16 @@ class rerollSplashScreen extends Phaser.State {
   }
 
   create() {
-    const text2 = this.add.text(this.game.width /10, this.game.height * 0.75 + 20, 'Love can be hard. Click to try again', {
+
+let message = "Love can be hard. Click to try again."
+if (this.game.global.numResets==1){
+  message = "Hang in there. There are plenty of satellites in space."
+}
+if (this.game.global.numResets==0){
+  message = "Maybe this is hopeless...I'll give it one more try"
+}
+
+    const text2 = this.add.text(this.game.width /10, this.game.height * 0.75 + 20, message, {
       font: '42px Arial', fill: '#ffffff', stroke: '#000000', strokeThickness: 6 , align: 'center'
     });
 
